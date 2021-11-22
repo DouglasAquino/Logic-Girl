072a467cd4cb6552dae40354902390e165e53ef1
Direção Acadêmica Adjunta <auxnae.pmw@ensino.ulbra.net>
2021-11-22 11:54:17 -0300

    formatação de textos aumotomatica

--- static/javascript/ajustetexto.js
+++ static/javascript/ajustetexto.js
@@ -1,39 +1,33 @@
+function ajustarTexto(elemento){
+    let teste = elemento.textContent.split("\n");
+    elemento.innerHTML = "";
+    let lista = document.createElement("ul");
+    teste.forEach(text => {
+        if(text.indexOf("<lista-item>") == -1){
+            let t = document.createElement("text");
+            t.textContent = text;
+            elemento.appendChild(t);
+        }else{
+            let li = document.createElement("li");
+            let b = document.createElement("b");
+            b.textContent = text.split("<lista-item>")[1]
+            li.appendChild(b);
+            lista.appendChild(li);
+            lista.appendChild(document.createElement("br"));
+            elemento.appendChild(lista);
+        }
+        elemento.appendChild(document.createElement("br"));
+    })
+}
+
 window.onload = () => {
     let corpo = document.querySelector(".pub-corpo-text");
-    let teste = corpo.textContent.split("\n");
-    corpo.innerHTML = "";
-    teste.forEach(text => {
-        let t = document.createElement("text");
-        t.textContent = text;
-        corpo.appendChild(t);
-        corpo.appendChild(document.createElement("br"));
-    })
+    ajustarTexto(corpo);
     let secao1 = document.querySelector(".pub-secao1").querySelector(".pub-corpo-text");
-    let sec = secao1.textContent.split("\n");
-    secao1.innerHTML = "";
-    console.log(sec)
-    sec.forEach(text => {
-        let t = document.createElement("text");
-        t.textContent = text;
-        secao1.appendChild(t);
-        secao1.appendChild(document.createElement("br"));
-    })
+    ajustarTexto(secao1);    
     let secao2 = document.querySelector(".pub-secao2").querySelector(".pub-corpo-text");
-    let sec2 = secao2.textContent.split("\n");
-    secao2.innerHTML = "";
-    sec2.forEach(text => {
-        let t = document.createElement("text");
-        t.textContent = text;
-        secao2.appendChild(t);
-        secao2.appendChild(document.createElement("br"));
-    })
+    ajustarTexto(secao2);    
     let refs = document.querySelector(".pub-referencias").querySelector(".pub-corpo-text");
-    let refst = refs.textContent.split("\n");
-    refs.innerHTML = "";
-    refst.forEach(text => {
-        let t = document.createElement("text");
-        t.textContent = text;
-        refs.appendChild(t);
-        refs.appendChild(document.createElement("br"));
-    })
+    ajustarTexto(refs);    
+    
 }
