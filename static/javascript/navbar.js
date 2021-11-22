1dc64c054fab4925c10b7320c1ab1cdb5ac5a59f
Direção Acadêmica Adjunta <auxnae.pmw@ensino.ulbra.net>
2021-11-22 14:35:59 -0300

    ajuste wrap

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -66,8 +66,44 @@ function TamanhoTela(){
     }
 }
 
+function ajustarTexto(elemento){
+    let teste = elemento.textContent.split("\n");
+    elemento.innerHTML = "";
+    let lista = document.createElement("ul");
+    teste.forEach(text => {
+        if(text.indexOf("<lista-item>") == -1){
+            let t = document.createElement("text");
+            t.textContent = text;
+            elemento.appendChild(t);
+            elemento.appendChild(document.createElement("br"));
+        }else{
+            let li = document.createElement("li");
+            let b = document.createElement("b");
+            b.textContent = text.split("<lista-item>")[1]
+            li.appendChild(b);
+            lista.appendChild(li);
+            lista.appendChild(document.createElement("br"));
+            elemento.appendChild(lista);
+        }
+    })
+}
+
+function formatarTexto(){
+    let corpo = document.querySelector(".pub-corpo-text");
+    ajustarTexto(corpo);
+    let secao1 = document.querySelector(".pub-secao1").querySelector(".pub-corpo-text");
+    ajustarTexto(secao1);    
+    let secao2 = document.querySelector(".pub-secao2").querySelector(".pub-corpo-text");
+    ajustarTexto(secao2);    
+    let refs = document.querySelector(".pub-referencias").querySelector(".pub-corpo-text");
+    ajustarTexto(refs);
+}
+
 window.onload = () => {
     TamanhoTela();
+    if(document.getElementById("publicacao")){
+        formatarTexto();
+    }
     document.querySelector(".btn-menu-mobile").addEventListener("click",abrirMenu);
     window.addEventListener("resize",TamanhoTela);
 }
